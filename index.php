1b5910cd5eb9922755f4f87dc64ccc6c929c84b3
realmaacke <mackan.pettersson1@outlook.com>
2022-02-16 10:46:05 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/6bfb37676a.js" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <title>Marcus Pettersson</title>
+</head>
+<body>
+    <div class="navigation">
+        <a class="active" href="#home"> <span id="nav-nmbrs">04. </span> Contact</a>
+        <a href="#news"><span id="nav-nmbrs">03. </span> Work</a>
+        <a href="#contact"><span id="nav-nmbrs">02. </span> Experience</a>
+        <a href="#about"><span id="nav-nmbrs">01. </span> About</a>
+    </div>
+
+    <div class="content">
+        <!-- LINKS TO OTHER PLATFORMS START -->
+        <div class="side">
+
+            <div class="side-item">
+            <a href="" id="icons-link">
+            <i class="fab fa-github"></i>
+            </a>
+            </div>
+
+            <div class="side-item">
+            <a href="" id="icons-link">
+            <i class="fab fa-codepen"></i>
+            </a>
+            </div>
+
+            <div class="side-item">
+            <a href="" id="icons-link">
+            <i class="fab fa-twitter"></i>
+            </a>
+            </div>
+
+            <div class="side-item">
+            <a href="" id="icons-link">
+            <i class="fab fa-instagram"></i>
+            </a>
+            </div>
+
+        </div>
+        <!-- END -->
+
+
+
+
+
+
+
+
+
+    </div>
+</body>
+</html>
