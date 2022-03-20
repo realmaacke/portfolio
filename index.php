b15357396a087e8e34881ec892bc05acdfe45cfb
realmaacke <mackan.pettersson1@outlook.com>
2022-03-20 06:04:08 +0100

    fix

--- index.php
+++ index.php
@@ -12,11 +12,11 @@
 <body>
     <div class="navigation">
         <a href="#04"> <span id="nav-nmbrs">04. </span> Contact</a>
-        <a href="#03"><span id="nav-nmbrs">03. </span> Work</a>
-        <a href="#02"><span id="nav-nmbrs">02. </span> Experience</a>
+        <a href="#03"><span id="nav-nmbrs">03. </span> Experience</a>
+        <a href="#02"><span id="nav-nmbrs">02. </span> Work</a>
         <a href="#01"><span id="nav-nmbrs">01. </span> About</a>
     </div>
-
+  <!-- 01 -->
     <div class="content" id="01">
         <!-- LINKS TO OTHER PLATFORMS START -->
         <div class="side">
@@ -50,11 +50,63 @@
 
 
         <div class="box01">
-
+            <p>Hi, my name is</p>
+            <h1>Marcus Pettersson</h1>
+            <h2>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor <br>
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud <br>
+            exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat. Duis aute irure <br>
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br>
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit. "
+            </h2>
+            <button class="fill" href="#03">Contact me</button>
         </div>
     </div>
     <!-- 02 -->
     <div class="content-02" id="02">
+
+        <div class="box02">
+
+            <div class="box02-item-l">
+                
+                <div class="box02-inner-item">
+
+                <a href="">
+                    
+                </a>
+
+                </div>
+
+                <div class="divider-h"></div>
+
+                <div class="box02-inner-item">
+
+                </div>
+
+            </div>
+
+            <div class="box02-item-r">
+
+                <div class="box02-inner-item">
+
+                </div>
+
+                <div class="divider-h"></div>
+
+                <div class="box02-inner-item">
+
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+    <!-- 03 -->
+    <div class="content-03" id="03">
+
+    </div>
+    <!-- 04 -->
+    <div class="content-04" id="04">
 
     </div>
 </body>
